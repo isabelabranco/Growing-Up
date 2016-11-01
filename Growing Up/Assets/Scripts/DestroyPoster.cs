50ab1fd2ebb41224abea8e0b3a9eb77b7256e798
Isabela Branco <bela.branco@gmail.com>
2016-11-01 11:45:14 -0200

    prototipo

---
+++ Growing Up/Assets/Scripts/DestroyPoster.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyPoster : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    public class Poster
+    {
+        public static int NumPosters = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(GetGameObjectClicked());
+        }
+    }
+    GameObject GetGameObjectClicked()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+        if (hit)
+        {
+            Poster.NumPosters--;
+            return hit.collider.gameObject;
+        }
+        return null;
+    }
+}
---
+++ Growing Up/Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTimer : MonoBehaviour {
+
+// Use this for initialization
+	void Start () {
+    }
+
+    float timeLeft = 20.0f;
+
+    // Update is called once per frame
+    void Update () {
+        timeLeft -= Time.deltaTime;
+
+        Debug.Log(timeLeft);
+        Debug.Log(global::DestroyPoster.Poster.NumPosters);
+
+        if (global::DestroyPoster.Poster.NumPosters == 0) {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Win");
+        }
+
+        if (timeLeft < 0 && global::DestroyPoster.Poster.NumPosters > 0) {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
+        }
+    }
+}
